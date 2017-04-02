ee443848adb0ba4b384712ff6a65b4be75bf3828
Dino Fancellu <dino@felstar.com>
2017-04-02 16:23:25 +0100

    Updated mapbox key to my own proper one, to fix stale key

--- example/src/main/scala/example/QuickStartLeaflet.scala
+++ example/src/main/scala/example/QuickStartLeaflet.scala
@@ -12,7 +12,7 @@ object QuickStartLeaflet extends {
 
     val lmap = L.map(el).setView((51.505, -0.09), 13)
 
-    val tileLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw",
+    val tileLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZmFuY2VsbHUiLCJhIjoiY2oxMHRzZm5zMDAyMDMycndyaTZyYnp6NSJ9.AJ3owakJtFAJaaRuYB7Ukw",
       TileLayerOptions.id("mapbox.streets").maxZoom(19).attribution("""Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,
                                                                       |<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
                                                                       |Imagery © <a href="http://mapbox.com">Mapbox</a>""".stripMargin))
